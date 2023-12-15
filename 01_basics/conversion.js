04872cc374f7f56c56dde9c7b4ba716c8880ff9d
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2023-12-15 14:17:21 +0000

    datatype conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,10 @@
+let value=22
+console.log(typeof value);
+new_value=Boolean(value)
+console.log(typeof new_value);
+console.log(new_value);
+
+ptr=true
+new_ptr=string(ptr)
+console.log(typeof ptr)
+console.log(new_ptr);

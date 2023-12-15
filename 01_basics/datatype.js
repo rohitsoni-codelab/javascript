92faa9e5d212419374dde8c96cd82e697769ebc9
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2023-12-15 14:01:07 +0000

    datatype01

---
+++ 01_basics/datatype.js
@@ -0,0 +1,23 @@
+const mobileNo=6204244981
+var Name="Ayush obishek"
+//try to avoid var, insted of it use let
+let cityName="Patna"
+
+console.log(Name);
+console.table([mobileNo,Name,cityName])
+
+
+/*type of datatypes in js are
+    number=>2 to the power53
+    bigint
+    string=>""
+    boolean=>true/false
+    null=>stand alone value
+    undefined
+    symbol=>unique
+    object
+*/
+
+console.log(typeof null)//object
+console.log(typeof"rohit soni");//string
+console.log(typeof undefined);//undefined

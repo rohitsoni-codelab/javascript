2d6acb7ef8b60e00728d7b1dde7e0b7b25997196
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-03-03 09:49:51 +0000

    basics datatypes

---
+++ Basics/02_datatype.js
@@ -0,0 +1,20 @@
+/*
+number
+bigint
+string
+boolean
+null
+undefined
+symbol=>always unique
+object
+*/
+
+console.log(typeof"Rohit");
+console.log(typeof 2.3);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof true);
+
+
+
+console.table(["rohit", 23.4,23, true])

78e829948331f29d807adeb78f8260582e92290c
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-03-03 11:05:22 +0000

    checked the prioriyies

---
+++ Basics/03_conversion.js
@@ -0,0 +1,24 @@
+let  num = true;
+// console.log(typeof num);
+// let stringNum=Number(num);
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+
+/*
+to Number
+number=> Number
+String=> NaN
+Boolean=> 0 1
+*/
+
+console.log(1+3+'2');
+console.log("1"+3+2)//priority matters.... lhs priority
+
+console.log('rohit' + 2)
+console.log(2+"rohit");
+
+console.log(true)
+console.log(+true)
+console.log(+"")
+

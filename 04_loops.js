6cb64058e7f2d1b9a726477adab9098973df4169
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-03-04 06:52:33 +0000

    while loop

--- 04_loops.js
+++ 04_loops.js
@@ -1,8 +1,16 @@
 //for loop in js
 
 let arr=[4,6,2,7,23,11]
-for(let i=1;i<arr.length;i++)
+for(let i=0;i<+arr.length;i++)
 {
     console.log(arr[i]);
     
+}
+// while loop
+console.log("\nWhile loop\n");
+let i=0
+while(i<arr.length)
+{
+    console.log(arr[i])
+    i++
 }

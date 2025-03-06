b9dd5af03a4d30f941f325ee3050942c322503f9
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-03-06 18:09:42 +0000

    forEach does not return anything

--- 05_array.js
+++ 05_array.js
@@ -1,14 +1,25 @@
 const arr=[1,2,3,4,5,6,7,8,9,10];
 // console.log(arr);
-arr.forEach((item,index)=>{
-    console.log(`Item ${item} at index ${index}`);
-}
-)
-// forEach(function(item){
-    //     console.log(item);
-    // })
-    
-// for (const elem of arr) {
-//     console.log(elem);
-    
+// arr.forEach((item,index)=>{
+//     console.log(`Item ${item} at index ${index}`);
 // }
+// )
+
+
+//doubling the values
+
+arr.forEach((item)=>{console.log(item*item)})//we can store this modified values in an array. Bcz forEach does not return any thing... it returns undefined
+//to re store the new value of the array in an array, we use map function
+
+
+
+
+
+// const crr=['Rohit','Raghav','Radha']
+// crr.forEach((item)=>{
+//     console.log(`Hello ${item}`)
+// })
+
+
+
+    

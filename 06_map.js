de1e582d350077dd373b1a823780a7b52c2e76a6
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-03-06 18:56:44 +0000

    changed to uppercase

---
+++ 06_map.js
@@ -0,0 +1,13 @@
+//CHANGING TO UPPER CASE...
+const nam=['rohit','raghv','kanhiya','radha']
+const upperNam=nam.map((item,index)=>
+{
+    console.log(`At index ${index} the value is ${item.toUpperCase()}`)
+    return `At index ${index} the value is ${item.toUpperCase()}`
+}
+)
+// console.log(upperNam)
+console.log("----------------------------------------------------")
+
+
+

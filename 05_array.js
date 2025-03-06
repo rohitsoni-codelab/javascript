4f119f90ccec0cce23ebf19ef008a610677a5dd3
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-03-06 17:15:28 +0000

    right way to iterate over a array in javascript

---
+++ 05_array.js
@@ -0,0 +1,14 @@
+const arr=[1,2,3,4,5,6,7,8,9,10];
+// console.log(arr);
+arr.forEach((item,index)=>{
+    console.log(`Item ${item} at index ${index}`);
+}
+)
+// forEach(function(item){
+    //     console.log(item);
+    // })
+    
+// for (const elem of arr) {
+//     console.log(elem);
+    
+// }

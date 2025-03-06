cdc23db048715b7a39a0d8b453466563849b5627
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-03-06 19:09:38 +0000

    accessing the array of object

--- 06_map.js
+++ 06_map.js
@@ -9,5 +9,16 @@ const upperNam=nam.map((item,index)=>
 // console.log(upperNam)
 console.log("----------------------------------------------------")
 
+//accessing key of values from the an array of object...
 
+const obj=[
+    {name: "Rohit", age:21},
+    {name: "Kishori", age:20},
+    {name: "Shyama", age:19},
+]
+
+const userName=obj.map((user,index)=>console.log(`Name=${user.name} and Age=${user.age}`))
+
+console.log("Converting the username into upper case................")
+const upperName=obj.map((user,index)=>console.log(`Name=${user.name.toUpperCase()} and Age=${user.age}`))
 

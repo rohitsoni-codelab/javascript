5e27aee8166770cb7c62f7d7110394bc5514327c
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-03-18 14:36:21 +0000

    local scope , global scope

---
+++ 08_scope.js
@@ -0,0 +1,8 @@
+{
+  let a = 59;
+  const b = 69;
+  var c = 79;
+}
+// console.log(a);// let has scope within its own block
+// console.log(b);// const has also its scope within its own block
+console.log(c);// var has no scope boundaries

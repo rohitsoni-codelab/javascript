79511662ccd33bbeeb1e901477f060ff2d101931
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-03-18 14:30:39 +0000

    passing object in function

--- 07_function.js
+++ 07_function.js
@@ -13,3 +13,19 @@ function showNumber(...num1){//========> can allow dynamically changable argumen
 }
 
 console.log(showNumber(499,533,488,199))
+
+
+
+
+
+//passing object in function
+const user={
+    name:"Rohit",
+    slary:94000
+}
+
+function showObj(anyobj){
+    console.log(`Username is ${anyobj.name} and user's salary is ${anyobj.slary}`)
+}
+
+showObj(user);

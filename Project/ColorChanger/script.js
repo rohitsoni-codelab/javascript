df19710c6d0a5d8f770dbd7d83ee001376569611
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-07-14 15:45:30 +0000

    color changer

--- Project/ColorChanger/script.js
+++ Project/ColorChanger/script.js
@@ -1,24 +1,37 @@
-// click on the color box to change the color of body to that color
-const button = document.querySelectorAll(".button");
-const bodyColor=document.querySelector("body")
+// // click on the color box to change the color of body to that color
+// const button = document.querySelectorAll(".button");
+// const bodyColor=document.querySelector("body")
 
-console.log(button);
-button.forEach((element) => {
-  element.addEventListener("click", function (e) {
+// console.log(button);
+// button.forEach((element) => {
+//   element.addEventListener("click", function (e) {
+//     console.log(e)
+//     console.log(e.target)
+//     if(e.target.id)
+//     {
+        
+//         bodyColor.style.backgroundColor=e.target.id;
+        
+//     }
+//   });
+// });
+// // const greet=document.querySelector(".greeting")
+// // setTimeout((e) => {
+// //     greet.style.display="block";
+// // }, 5000);
+
+
+const button=document.querySelectorAll('.button')
+const bodyColor=document.querySelector('body')
+
+button.forEach(function(element){
+  element.addEventListener('click', function(e){
     console.log(e)
     console.log(e.target)
     if(e.target.id)
     {
-        
-        bodyColor.style.backgroundColor=e.target.id;
-        
+      bodyColor.style.backgroundColor=e.target.id;
     }
-  });
-});
-// const greet=document.querySelector(".greeting")
-// setTimeout((e) => {
-//     greet.style.display="block";
-// }, 5000);
-
-
+  })
+})
 
